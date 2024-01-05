1f1bc61ba4b0e9b9ebf5907ad77810a0ca24358c
Radoslaw Lyson <radoslawlyson@gmail.com>
2024-01-05 11:59:49 +0100

    feat: add simple CRUD for charging station type

--- src/charging_station_type/charging_station_type.controller.ts
+++ src/charging_station_type/charging_station_type.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { ChargingStationTypeService } from './charging_station_type.service';
+import { ChargingStationType } from './charging_station.entity';
+import { CreateChargingStationTypeDTO } from './dto/charging_station_type_create_dto';
+import { UpdateChargingStationTypeDTO } from './dto/charging_station_type_update_dto';
 
 @Controller('charging-station-type')
-export class ChargingStationTypeController {}
+export class ChargingStationTypeController {
+    constructor(private readonly chargingStationTypeService: ChargingStationTypeService) {}
+
+    @Get()
+    async findAll(): Promise<ChargingStationType[]> {
+        return this.chargingStationTypeService.findAll();
+    }
+
+    @Get(':id')
+    async findById(@Param('id') id: string): Promise<ChargingStationType> {
+        return this.chargingStationTypeService.findById(id);
+    }
+
+    @Post()
+    async create(@Body() chargingStationTypeDTO: CreateChargingStationTypeDTO) {
+        await this.chargingStationTypeService.create(chargingStationTypeDTO);
+    }
+
+    @Delete(':id')
+    async remove(@Param('id') id: string) {
+        await this.chargingStationTypeService.remove(id);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: string, @Body() updateChargingStationType: UpdateChargingStationTypeDTO) {
+        this.chargingStationTypeService.update(id, updateChargingStationType);
+    }
+}
