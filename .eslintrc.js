64567fdfde48afad2b50ec617e33ba90eeee5014
Radoslaw Lyson <radoslawlyson@gmail.com>
2024-01-10 10:33:08 +0100

    fix: add return-await forcing for eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,8 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'no-return-await': 'off',
+    "@typescript-eslint/return-await": ["error", "always"],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
