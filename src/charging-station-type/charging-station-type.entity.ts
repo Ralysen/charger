a62d0cbbfa38e890ae3a1e4eafd971cb478ed675
Radoslaw Lyson <radoslawlyson@gmail.com>
2024-01-10 10:57:37 +0100

    fix: entity code readability fix (charging-station-type)

--- src/charging-station-type/charging-station-type.entity.ts
+++ src/charging-station-type/charging-station-type.entity.ts
@@ -1,5 +1,8 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-import { ChargingStationTypeCurrentType, CurrentType } from './enums/charging-station-type.current-type';
+import {
+  ChargingStationTypeCurrentType,
+  CurrentType,
+} from './enums/charging-station-type.current-type';
 import { ChargingStation } from 'src/charging-station/charging-station.entity';
 
 @Entity()
