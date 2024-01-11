64c3a778537d9349aaee71ef8e7ee1bc1b416ad3
Radoslaw Lyson <radoslawlyson@gmail.com>
2024-01-11 15:43:29 +0100

    fix: HttpExceptionFilter fix

--- src/response-handling/http-exception-filter.ts
+++ src/response-handling/http-exception-filter.ts
@@ -4,21 +4,18 @@ import {
   ExceptionFilter,
   HttpException,
 } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
 
     response.status(status).json({
-      statusCode: status,
-      message: exception.message,
+      errors: exception.getResponse(),
       timestamp: new Date().toISOString(),
-      path: request.url,
     });
   }
 }
